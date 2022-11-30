2c0dd30bc5417bfb8c5cc6f95d968a58151be2dd
mddo <mddo>
2022-11-30 23:56:45 +0000

    update delete config

--- project/remove_clab.sh
+++ project/remove_clab.sh
@@ -1,5 +1,9 @@
 #source ./.env
 source ../../.env
+rm -f ./playbooks/pushed_configs_mddo_network.json
+rm -f ./playbooks/*conf
+rm -f ./playbooks/clab-topo.yml
 cd $LOCALSERVER_HOMEDIR
 sudo containerlab destroy --topo /tmp/clab-topo.yml --cleanup
-
+sudo rm -f /tmp/*conf
+sudo rm -f /tmp/clab-topo.yml
