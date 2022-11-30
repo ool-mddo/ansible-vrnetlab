c14ed73e2db320c1fc3e1305e440465a65f1e6f9
mddo <mddo>
2022-11-30 18:02:42 +0000

    update ospf jinja2 & netomox job

--- project/create_emulated_asis.sh
+++ project/create_emulated_asis.sh
@@ -17,4 +17,4 @@ cd $DEMO_DIR/emulated_tobe/configs
 DIR=$LOCALSERVER_HOMEDIR/clab-$LABNAME; ls $DIR/*/config | grep region | rev | cut -d "/" -f 2 | rev | xargs -INAME bash -c "cat $DIR/NAME/config/juniper.conf > NAME.conf"
 
 cd $PLAYGROUND_DIR
-sudo docker-compose run netomox-exp bundle exec rake NETWORK=${NETWORK_NAME}
+sudo docker-compose run netomox-exp bundle exec rake NETWORK=${NETWORK_NAME} PHY_SS_ONLY=1
--- project/create_emulated_tobe.sh
+++ project/create_emulated_tobe.sh
@@ -10,7 +10,8 @@ cd $DEMO_DIR/emulated_tobe/configs
 DIR=$LOCALSERVER_HOMEDIR/clab-$LABNAME; ls $DIR/*/config | grep region | rev | cut -d "/" -f 2 | rev | xargs -INAME bash -c "cat $DIR/NAME/config/juniper.conf > NAME.conf"
 
 cd $PLAYGROUND_DIR
-sudo docker-compose run netomox-exp bundle exec rake NETWORK=$NETWORK_NAME
+sudo docker-compose run netomox-exp bundle exec rake NETWORK=$NETWORK_NAME  PHY_SS_ONLY=1
+
 
 
 cd $MODEL_MERGE_DIR/model_merge
