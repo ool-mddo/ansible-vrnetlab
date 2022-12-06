7e6989c2d0ed76222fb6c0a287f0085a044cb948
mddo <mddo>
2022-12-06 02:55:58 +0000

    update demo script

--- demo_remove.sh
+++ demo_remove.sh
@@ -7,5 +7,8 @@
 
 source ./demo_vars
 
-sudo ansible-runner run . -p /data/project/playbooks/remove.yml --container-option="--net=${NODERED_BRIDGE}" --container-volume-mount="$PWD:/data" --container-image=${ANSIBLERUNNER_IMAGE}:latest --process-isolation --process-isolation-executable docker --cmdline "-e nodered_url=${NODERED_URL} -e labname=${LABNAME} -e login_user=${LOCALSERVER_USER} -e netoviz_url=${NETVIZ_URL} -e network_name=${NETWORK_NAME} -e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -k -K "
+sudo ansible-runner run . -p /data/project/playbooks/remove.yml --container-option="--net=${NODERED_BRIDGE}" \
+	--container-volume-mount="$PWD:/data" --container-image=${ANSIBLERUNNER_IMAGE}:latest \
+	--process-isolation --process-isolation-executable docker --cmdline \
+	"-e nodered_url=${NODERED_URL} -e labname=${LABNAME} -e login_user=${LOCALSERVER_USER} -e netoviz_url=${NETVIZ_URL} -e network_name=${NETWORK_NAME} -e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -k -K "
 
--- demo_step4.sh
+++ demo_step4.sh
@@ -8,5 +8,8 @@
 source ./demo_vars
 
 cd $MODEL_MERGE_DIR/model_merge
-python3.10 merge.py config $PLAYGROUND_DIR/netoviz_model/$NETWORK_NAME/original_asis/topology.json $PLAYGROUND_DIR/netoviz_model/$NETWORK_NAME/emulated_asis/topology.json $PLAYGROUND_DIR/netoviz_model/$NETWORK_NAME/emulated_tobe/topology.json | jq '.[].config' | xargs -ICMD echo -e CMD
+python3.10 merge.py config \
+	$PLAYGROUND_DIR/netoviz_model/$NETWORK_NAME/original_asis/topology.json \
+	$PLAYGROUND_DIR/netoviz_model/$NETWORK_NAME/emulated_asis/topology.json \
+	$PLAYGROUND_DIR/netoviz_model/$NETWORK_NAME/emulated_tobe/topology.json | jq '.[].config' | xargs -ICMD echo -e CMD
 
