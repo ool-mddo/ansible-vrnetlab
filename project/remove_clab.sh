fa0f9b220280fc5b838f4fa345bc0736d99f2595
t-tkgh <toshiyuki.takiguchi@okinawaopenlabs.org>
2022-12-08 02:10:39 +0900

    add playbooks/configs

--- project/remove_clab.sh
+++ project/remove_clab.sh
@@ -1,8 +1,7 @@
 source ../demo_vars
 #source ../../.env
-rm -f $ANSIBLE_RUNNER_DIR/project/playbooks/pushed_configs_mddo_network.json
-rm -f $ANSIBLE_RUNNER_DIR/project/playbooks/*conf
-rm -f $ANSIBLE_RUNNER_DIR/project/playbooks/clab-topo.yml
+rm -f $ANSIBLE_RUNNER_DIR/project/playbooks/configs/*conf
+rm -f $ANSIBLE_RUNNER_DIR/project/playbooks/configs/clab-topo.yml
 cd $LOCALSERVER_HOMEDIR
 sudo containerlab destroy --topo /tmp/clab-topo.yml --cleanup
 sudo rm -f /tmp/*conf
