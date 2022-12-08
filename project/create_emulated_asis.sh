6876a58f7c32351c8b6bf7a6bdc81c446088e851
mddo <mddo>
2022-12-08 03:08:33 +0000

    update step02/03

--- project/create_emulated_asis.sh
+++ project/create_emulated_asis.sh
@@ -5,8 +5,8 @@ rm -rf $DEMO_DIR/emulated_asis/configs/*
 rm -rf $DEMO_DIR/emulated_tobe/configs/*
 rm -rf $DEMO_DIR/emulated_asis/hosts/*
 rm -rf $DEMO_DIR/emulated_tobe/hosts/*
-cp playbooks/Seg*conf $DEMO_DIR/emulated_asis/configs/
-cp playbooks/Seg*conf $DEMO_DIR/emulated_tobe/configs/
+cp playbooks/configs/Seg*conf $DEMO_DIR/emulated_asis/configs/
+cp playbooks/configs/Seg*conf $DEMO_DIR/emulated_tobe/configs/
 
 original_asis=`curl ${NETOVIZ_URL}/model/${NETWORK_NAME}/original_asis/topology.json`
 curl -X 'POST'   'http://localhost:1880/batfish_l1topo'   -H 'accept: application/json'   -H 'Content-Type: application/json'  --data "$original_asis" > $DEMO_DIR/emulated_asis/batfish/layer1_topology.json
