4c4bcb7b0c9d90b146b30a5ceed4519900c3c934
mddo <mddo>
2022-12-09 07:55:28 +0000

    update step1/2

--- demo_step2.sh
+++ demo_step2.sh
@@ -7,7 +7,13 @@
 
 source ./demo_vars
 
-sudo ansible-runner run . -p /data/project/playbooks/step02.yml --container-option="--net=${NODERED_BRIDGE}" \
+sudo ansible-runner run . -p /data/project/playbooks/step02-1.yml --container-option="--net=${NODERED_BRIDGE}" \
+	--container-volume-mount="$PWD:/data" --container-image=${ANSIBLERUNNER_IMAGE} \
+       	--process-isolation --process-isolation-executable docker --cmdline  \
+	"-e nodered_url=${NODERED_URL} -e labname=${LABNAME} -e login_user=${LOCALSERVER_USER} -e netoviz_url=${NETOVIZ_URL} -e network_name=${NETWORK_NAME} -k -K "
+
+
+sudo ansible-runner run . -p /data/project/playbooks/step02-2.yml --container-option="--net=${NODERED_BRIDGE}" \
        	--container-volume-mount="$PWD:/data" --container-image=${ANSIBLERUNNER_IMAGE} \
 	--process-isolation --process-isolation-executable docker --cmdline \
 	"-e nodered_url=${NODERED_URL} -e labname=${LABNAME} -e login_user=${LOCALSERVER_USER} -e netoviz_url=${NETVIZ_URL} -e network_name=${NETWORK_NAME} -e ansible_runner_dir=${ANSIBLE_RUNNER_DIR} -k -K "
