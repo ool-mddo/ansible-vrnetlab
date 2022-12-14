41b22759bd0062ed8bcbabdcc6a62a6e82437d65
t-tkgh <toshiyuki.takiguchi@okinawaopenlabs.org>
2022-12-14 18:00:09 +0900

    update created_emulated_asis/tobe

--- project/test.sh
+++
@@ -1,8 +0,0 @@
-#source ./.env
-source ../demo_vars
-
-
-cd $DEMO_DIR/emulated_tobe/configs
-#DIR=$LOCALSERVER_HOMEDIR/clab-$LABNAME;  find  $DIR/*/config  -name juniper.conf 
-DIR=$LOCALSERVER_HOMEDIR/clab-$LABNAME;  find  $DIR/*/config  -name juniper.conf | rev | cut -d "/" -f 3 | rev 
-#DIR=$LOCALSERVER_HOMEDIR/clab-$LABNAME; ls $DIR/*/config | grep -e "juniper.conf" | rev | cut -d "/" -f 2 | rev 
